1f6e65f4bcf36e45e88937c4676b01102c39d692
Vinula Senarathne <vinulapc@gmail.com>
2024-10-16 09:04:46 +0530

    feat: 3d model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -3,13 +3,14 @@ import Lights from "./Lights";
 import { Suspense } from "react";
 import IPhone from "./IPhone";
 import * as THREE from "three";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -17,7 +18,7 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`w-full h-full ${index == 2 ? "right-[-100%]" : ""}`}
+      className={`w-full h-full absolute ${index == 2 ? "right-[-100%]" : ""}`}
     >
       <ambientLight intensity={0.3} />
 
@@ -40,7 +41,7 @@ const ModelView = ({
         name={`${index === 1} ? 'small' : 'large'`}
         position={[0, 0, 0]}
       >
-        <Suspense fallback={<mesh>Loading</mesh>}>
+        <Suspense fallback={<Loader />}>
           <IPhone
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
